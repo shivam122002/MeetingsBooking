79271851f01820652562b78ca6ad3b051c66b74a
Shivam Devkota <shivamdevkota001@gmail.com>
2026-07-11 01:46:29 +0530

    Book meetings UI completed

---
+++ MeetingsBooking.Shared/Dtos/BookMeetingsDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingsBooking.Shared.Dtos
+{
+    public class BookMeetingsRequestDto
+    {
+        public DateTime MeetingDate { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+
+        public int DurationInMinutes { get; set; }
+
+        public string TimeZone { get; set; } = string.Empty;
+
+        public string MeetingDetailsJson { get; set; } = string.Empty;
+    }
+    public class MeetingDetailsDto
+    {
+        public string MeetingTitle { get; set; } = string.Empty;
+
+        public string MeetingReason { get; set; } = string.Empty;
+
+        public List<string> DiscussionTopics { get; set; } = [];
+
+        public string AdditionalNotes { get; set; } = string.Empty;
+    }
+    public class BookMeetingsResponse
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }   
+}
