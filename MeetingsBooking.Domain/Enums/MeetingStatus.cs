3942fc235243df3e397000a5327146fe505dbe6a
Shivam Devkota <shivamdevkota001@gmail.com>
2026-07-11 23:25:01 +0530

    Done...

---
+++ MeetingsBooking.Domain/Enums/MeetingStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingsBooking.Domain.Enums
+{
+    public enum MeetingStatus
+    {
+        Scheduled = 1,      // Booking created
+
+        Pending = 2,        // Waiting for host approval
+
+        Accepted = 3,       // Host accepted
+
+        InProgress = 4,     // Meeting is live
+
+        Completed = 5,      // Meeting finished
+
+        Cancelled = 6,      // Cancelled
+
+        Expired = 7         // Missed / never joined
+    }
+}
