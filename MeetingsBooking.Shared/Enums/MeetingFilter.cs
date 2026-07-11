42a038cb41f6f18b8cab48a0b6c79902b06d7461
Shivam Devkota <shivamdevkota001@gmail.com>
2026-07-11 19:06:55 +0530

    Added filteres and join meeting button

---
+++ MeetingsBooking.Shared/Enums/MeetingFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeetingsBooking.Shared.Enums
+{
+    public enum MeetingFilter
+    {
+        All,
+        Upcoming,
+        Pending,
+        Past,
+        Cancelled
+    }
+}
