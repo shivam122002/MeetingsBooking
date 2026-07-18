cfe39ed1179064a54559516ea09755eb8ead2a4f
Shivam Devkota <shivamdevkota001@gmail.com>
2026-07-18 17:08:56 +0530

    added key vault part

--- MeetingsBooking.API/Program.cs
+++ MeetingsBooking.API/Program.cs
@@ -101,11 +101,11 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
 }
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+}
 
 app.UseHttpsRedirection();
 app.UseCors("UI");
